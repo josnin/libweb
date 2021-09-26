83815600da152fe93b947e04ce91d264fdd06171
jose <nino.capati@gmail.com>
2021-09-26 12:02:10 +0800

    cleanup

--- src/static/js/components/data-binding-2way.js
+++ src/static/js/components/data-binding-2way.js
@@ -2,20 +2,22 @@ const template = document.createElement('template');
 template.innerHTML = `
 <div>
 <input data-bind="username" type="text" />
+<input data-bind="lastname" type="text" />
 </div> 
 <div>{username} XYZ</div>
 <div>{username} This is ates???</div>
 <div>{username} This is ates??? {username}</div>
 
-<!-- what if multiple data-bind??? -->
-<div data-bind="username">{username} This is ates??? {lastname}</div>
-<button data-bind="username">Click me?</button>
+<div>{username} This is ates??? {lastname}</div>
+<button>Click me {lastname}?</button>
 `
 
 import {
   makeReactive,
   toSimulate,
 } from '../toJS.js';
+
+
 
 class DataBinding2Way extends HTMLElement {
 
@@ -25,21 +27,27 @@ class DataBinding2Way extends HTMLElement {
       this.shadowRoot.appendChild(template.content.cloneNode(true));
 
       this.properties = makeReactive(
-        {
+        // need to know the shadow DOM
+        this.shadowRoot, 
+
+        // declare reactive variables here
+        { 
           username: 'darling',
           lastname: 'javascript'
-        },
-        this.shadowRoot
+        }
+
       );
 
 
-      //toSimulate(
-      //  this.properties,
-      //  this.shadowRoot
-      //)
+      toSimulate(
+        this.properties,
+        this.shadowRoot
+      )
 
 
     }
+
+
 
 }
 
