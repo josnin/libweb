c02f64a8b8ca237084664dd82c4c456edd0cbc99
Jose <nino.capati@gmail.com>
2022-04-15 16:27:28 +0800

    cleanup

--- src/reactive.ts
+++ src/reactive.ts
@@ -1,50 +1,50 @@
 
 export const createReactive = (
-  self: any, 
-  varObj: any, 
+  self: any,
+  varObj: any,
   events: any,
   templates: any,
 ) => {
   // react when there is a changes in value
-  //const allElements = self.shadowRoot.querySelectorAll('[data-bind]');
-  const allElements = self.shadowRoot.querySelectorAll("*");
+  // const allElements = self.shadowRoot.querySelectorAll('[data-bind]');
+  const allElements = self.shadowRoot.querySelectorAll('*');
   const handler = {
     get: (varObj: any, prop: string) => {
       return varObj[prop] ;
     },
     set: (varObj: any, prop: string, value: string) => {
       allElements.forEach((element: any) => {
-        if (element.type == 'text' && 
-        element.dataset.bind == prop) {
+        if (element.type === 'text' &&
+        element.dataset.bind === prop) {
           // make sure to update only that match with data-binding
           element.value = value;
         } else {
           // {username} > johny<!--{username}-->
           templates.updateVarHTMLOnChange(
-            element, 
-            varObj, 
-            prop, 
+            element,
+            varObj,
+            prop,
             value
           );
 
           templates.updateVarAttrOnChange(
-            element, 
-            prop, 
+            element,
+            prop,
             value
           );
 
           events.createEventListener(self);
         }
-      })
+      });
       varObj[prop] = value;
-      return true; 
+      return true;
     }
-  }
+  };
 
   return new Proxy(varObj, handler);
-}
+};
 
 
 export default {
   createReactive,
-}
+};
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 // (variable) -> variable
 export const stripParenthesis = (value: string) => {
-  return value[0].replace(/[()]/g, ''); 
+  return value[0].replace(/[()]/g, '');
 };
 
 
@@ -11,12 +11,12 @@ export const strip = (value: string, start: string, end: string) => {
 
 
 export const addQuote = (val: any) => {
-  if (isNaN(val) && !val.startsWith("'")) {
+  if (isNaN(val) && !val.startsWith('\'')) {
     return `'${val}'`;
   } else {
     return val;
   }
-}
+};
 
 // surround args multiple value with quote
 export const addQuoteItems = (value: any[]) => {
@@ -35,4 +35,4 @@ export default {
   strip,
   addQuote,
   addQuoteItems
-}
+};
