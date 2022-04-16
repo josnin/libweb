c05df7342f96d651cf4c321776b5066cc24fbc7a
Jose <nino.capati@gmail.com>
2022-04-16 22:31:22 +0800

    cleanup

--- src/directives/if.directives.js
+++
@@ -1,7 +0,0 @@
-export const If = (self, el) => {
-    const _If = el.getAttribute('_if');
-    if (_If && !self[_If] && !self.__reactive[_If]) {
-        el.style.display = 'none';
-    }
-    return el;
-};
