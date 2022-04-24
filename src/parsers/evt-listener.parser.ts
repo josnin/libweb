81b183f0494c87dccfeb6c86bdd6a26d3bc8873d
Jose <nino.capati@gmail.com>
2022-04-24 09:44:15 +0800

    evt to accept non func value

--- src/parsers/evt-listener.parser.ts
+++ src/parsers/evt-listener.parser.ts
@@ -4,7 +4,13 @@ export const evtListenerParser = (self: any, el: any) => {
   Array.from(el.attributes).map( (e: any) => {
       if (e.name.startsWith(settings.ATTR_PREFIX)) {
         el[e.name.replace(settings.ATTR_PREFIX, 'on')] = ($event: any) => {
-           eval(`self.${e.value.replaceAll('\'$event\'', '$event')}`); // execute function
+          try {
+            // exec normal func
+            eval(`self.${e.value.replaceAll('\'$event\'', '$event')}`); 
+          } catch {
+            // exec direct js, i.e alert(1)
+            eval(e.value);
+          }
         } 
       }
   })
