770bb00a19446d9837916eb27f3b78e4261a1272
Jose <nino.capati@gmail.com>
2022-05-17 22:36:56 +0800

    bollean, number attr parser

--- src/parsers/attr.parser.ts
+++ src/parsers/attr.parser.ts
@@ -1,29 +1,28 @@
-import { getVal, isFn, getFnVal } from "../utils.js";
+import { getVal, isFn, getFnVal } from '../utils.js';
 
 export const attrParser = async (self: any, el: HTMLElement) => {
-  //const [self, el] = args; // @todo handling self any el HTML element??
+  // const [self, el] = args; // @todo handling self any el HTML element??
   const attrObj: any = {};
   for (const [_, attr] of Object.entries(el.attributes)) {
 
     if (attr.name.startsWith(':')) {
       const fName = attr.name.split(':')[1];
 
-      const { res, get } = getVal(self, attr.value); 
-      if (get && res && typeof(res) === 'string') { // string
-        el.setAttribute(fName, res); 
-
-      } else if (!get && !isFn(res)) {  // boolean
-        // inline js, not found in the variable
+      const { res, get } = getVal(self, attr.value);
+      if (get && res && ['string', 'number'].includes(typeof(res))) { // string, numeric
+        el.setAttribute(fName, res);
+      } if (get && res && typeof(res) === 'boolean') { // boolean
+        el.setAttribute(fName, '');
+      } else if (!get && !isFn(res)) {  // boolean inline js
         const fVal = Function(`return ${res}`)();
         if (fVal && typeof(fVal) === 'boolean') { el.setAttribute(fName, ''); }
-
       } else if (!get && isFn(res)){ // function
         const { fnVal } = getFnVal(self, el, res);
-        el.setAttribute(fName, fnVal)
+        el.setAttribute(fName, fnVal);
       }
 
-      el.removeAttribute(attr.name)
-      attrObj[fName] = attr.value
+      el.removeAttribute(attr.name);
+      attrObj[fName] = attr.value;
     }
   }
   if (Object.keys(attrObj).length > 0 ) {
