3fdc6ebdf5899e23b864367f43133519ffff4045
josnin <nino.capati@gmail.com>
2022-05-18 22:50:43 +0800

    Update sample-binding.js

--- samples/sample-binding.js
+++ samples/sample-binding.js
@@ -1,9 +1,9 @@
-import { LWElement } from '../libweb.js';
-class BindingComponent extends LWElement {
+import { LWElement } from "./libweb.js";
+class Binding extends LWElement {
     constructor() {
         super(...arguments);
         this.__reactive = { username: 'darling' };
-        this.__template = `<input data-bind="username" type="text"/> {username}`;
+        this.__template = `<input (model)="username" type="text"/> {username}`;
     }
 }
-customElements.define('sample-binding', BindingComponent);
+customElements.define('sample-binding', Binding);
